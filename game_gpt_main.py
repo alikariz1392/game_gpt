2e642d81a54b5f20efc8c129969f141411a640c9
ali-kariz <majid1140@gmail.com>
2023-12-19 15:30:36 +0330

    making simple chat gpt

--- game_gpt_main.py
+++ game_gpt_main.py
@@ -3,7 +3,11 @@ import pygame as py
 import openai as ai
 import time as  tm
 # setup
-ai.api_key = "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"
-player = input(">>> ")
-response = ai.ChatCompletion.create(model = "gpt-3.5-turbo" , massages=[{"role":"user" , "content":player }])
-print(response.choices[0].massages.content)
+ai.api_key = "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"
+breakwhile = True
+while breakwhile:
+    player = input(">>> ")
+    response = ai.ChatCompletion.create(model = "gpt-3.5-turbo" , messages=[{"role":"user" , "content":player }])
+    print(response.choices[0].message.content)
+    if player in ["exit", "goodbye", 'bye']:
+        breakwhile = False
