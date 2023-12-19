647e7a9352b3af132e1a1c15e88f9cdd9352478a
ali-kariz <majid1140@gmail.com>
2023-12-19 14:46:26 +0330

    adding the setup and changing version

--- game_gpt_main.py
+++ game_gpt_main.py
@@ -1,5 +1,9 @@
 # imports
-import pygame
-import openai
+import pygame as py
+import openai as ai
+import time as  tm
 # setup
-openai.iapi_key = "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"
+ai.api_key = "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"
+player = input(">>> ")
+response = ai.ChatCompletion.create(model = "gpt-3.5-turbo" , massages=[{"role":"user" , "content":player }])
+print(response.choices[0].massages.content)
